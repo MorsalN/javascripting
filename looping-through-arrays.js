a7a746b3b21073b701685b9c1085ae17791d32ae
Morsal Niyaz <morsalniyaz@gmail.com>
2021-09-30 10:17:07 -0700

    Initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,13 @@
+var pets = ['cat','dog','rat'];
+for (var i =0; i<pets.length; i++) {
+pets[i] = pets[i] + 's';
+};
+console.log(pets);
+
+// IF YOU PUT RETURN IT DOES NOT WORK
+// var pets = ['cat', 'dog', 'rat'];
+
+// for (var i=0;i < pets.length; i++){
+// return pets[i] = pets[i] + 's';
+// }
+// console.log(pets)
